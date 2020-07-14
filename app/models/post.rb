f8f13e4d24fb95a374b9f6081f7a2243774e1abc
koshigoe <koshigoe.toshiki@gmail.com>
2020-07-14 21:15:50 +0000

    add validates image

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   
   validates :title, presence: true, length: { maximum: 10 }
+  validates :image, presence: true
   #validates :image_name, presence: true
   mount_uploader :image, ImageUploader
   
